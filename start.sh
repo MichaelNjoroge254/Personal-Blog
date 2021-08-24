7e7b85e9969748ddf05891cb9e87084ca850c58f
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-08-24 09:39:56 +0300

    modified start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='123125'
+export DATABASE_URI = 'postgresql+psycopg2://moringa:password@localhost/Personal-Blog'
+export MAIL_USERNAME='michael.m.njoroge254@gmail.com'
+export MAIL_PASSWORD='Santo'
+
+python3.8 manage.py server
