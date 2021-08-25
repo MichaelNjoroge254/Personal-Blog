91118154e2784151cb73bf5998858cfe876337ee
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-08-25 19:41:12 +0300

    deploy to heroku

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='2001c'
-export MAIL_USERNAME='cynthiaobu940@gmail.com'
-export MAIL_PASSWORD='21038646'
+export MAIL_USERNAME='michael.m.njoroge254@gmail.com'
+export MAIL_PASSWORD='Michinza254'
 
 python3 manage.py server
